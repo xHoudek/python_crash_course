223064cd7d32e6d218f64c5c99c3104e0dee4191
xhoudek <xhoudek@gmail.com>
2019-03-25 19:53:24 -0500

    added buffet.py

---
+++ chapter_3/buffet.py
@@ -0,0 +1,10 @@
+menu = ('pizza', 'fried chicken', 'ice cream', 'ham', 'wontons')
+
+for food in menu:
+    print(food)
+
+#menu[0] = 'salmon' will not work
+
+new_menu = ('pizza', 'waffles', 'ice cream', 'sushi', 'wontons')
+for food in new_menu:
+    print(food)
