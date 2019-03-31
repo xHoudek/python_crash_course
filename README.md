8feb02163e66bb85d86074092ab1493d038ad125
xhoudek <xhoudek@gmail.com>
2019-03-31 13:03:57 -0500

    Added some additional info to Readme. Added chapter 5 practice

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #python_crash_course
+
+These are all of my practice files for the book Python Crash Course by Eric Matthes.
+I am also learning Git/Github at the same time as working on this book.
---
+++ chapter_5/conditional_tests.py
@@ -0,0 +1,26 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False")
+print(car == 'audi')
+
+number = 8**2
+print("Is number == 64? I predict True")
+print(number == 64)
+
+print('\nIs number == 16? I predict False')
+print(number == 16)
+
+print('\nIs number >= 0? I predict True')
+print(number >= 0)
+
+print('\nIs number != 16? I predict False')
+print(number != 16)
+
+a_list = [0,1,2,3,4,5]
+print('\nIs 3 in a_list? I predict True')
+print(3 in a_list)
+
+print('\nIs 9001 in a_list? I predict False')
+print(9001 in a_list)
